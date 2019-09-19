ccb9237de22d98bd42639053eb33759118a4ace0
David Balbas <davebg31@gmail.com>
2019-09-19 11:13:07 +0200

    Update folder creation

--- create_folders.py
+++ create_folders.py
@@ -1,1 +1,14 @@
-create folders
+import os
+	
+try:
+    os.makedirs('7mindata/npymats')
+    os.makedirs('7mindata/real_planck')
+    os.makedirs('7mindata/fullmaps')
+    os.makedirs('5mindata/npymats')
+    os.makedirs('5mindata/fullmaps')
+    os.makedirs('5minplanedata/npymats')
+    os.makedirs('5minplanedata/fullmaps')
+	    
+    print("Creation succesful")
+except FileExistsError:
+    print("Directory/ies already exists") 
